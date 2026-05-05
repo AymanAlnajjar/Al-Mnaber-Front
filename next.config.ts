f9b5398e31baf4d64ac578525103c628511a1acd
Ayman Alnajjar <103314139+AymanAlnajjar@users.noreply.github.com>
2026-05-05 12:41:35 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,40 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  compress: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.almnaber.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "api.almnaber.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
